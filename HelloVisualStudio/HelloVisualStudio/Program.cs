d82f56c7fd534bd1a51266989bd71b62342b7e15
Jonathan <jdela136@gmail.com>
2021-02-10 21:22:21 -0500

    added Hello Studio Code  gitignore update

---
+++ HelloVisualStudio/HelloVisualStudio/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Humanizer;
+
+namespace HelloVisualStudio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inventory App 1.0");
+            Console.Write("What item do you have?");
+            string item = Console.ReadLine();
+            Console.Write("How do you have?");
+            int quantity = int.Parse(Console.ReadLine());
+
+            if(quantity != 1)
+            {
+                item = Pluralize(item);
+            }
+
+            Console.WriteLine($"You have {quantity} {item}");
+        }
+        static string Pluralize(string item)
+        {
+            return item.Pluralize();
+        }
+    }
+}
